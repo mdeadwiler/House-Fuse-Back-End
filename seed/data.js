dcfd3453560aa80af0b37022fc1e339833fa42bd
MDeadwiler <mdeadwiler@email.com>
2024-10-24 21:47:06 -0400

    init commit

---
+++ seed/data.js
@@ -0,0 +1,9 @@
+import db from "../db/connection.js";
+
+
+// Any personal data you want to add to the database
+await db.close();
+
+
+
+//insertData();
---
+++ server.js
@@ -0,0 +1,26 @@
+import db from "./db/connection.js"// let db = mongoose.connection
+import express from "express";
+import logger from "morgan";
+import chalk from "chalk";
+import dotenv from "dotenv"
+dotenv.config();
+import cors from "cors";
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+// Routes go here
+
+
+
+
+db.on("connected", () => {
+    console.clear()
+    console.log(chalk.blue("Connected to MongoDB!"));
+    app.listen(PORT, () => {
+      console.log(`Express server running on port: ${PORT}`);
+    });
+})
