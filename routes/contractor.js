a33c80dbffdadccf43c989c39a3f9354730d096a
MDeadwiler <mdeadwiler@email.com>
2024-10-25 16:44:24 -0400

    safe-backEnd-Works

--- routes/contractor.js
+++ routes/contractor.js
@@ -1,7 +1,7 @@
-//import { Router } from "express";
+import { Router } from "express";
 // import * as controllers from "../controllers/contractor.js";
 
-
+const router = Router();
 
 // POST /homeowners - for creating a homeowner.
 // POST /contractors - for creating a contractor.
@@ -9,3 +9,5 @@
 // GET /jobs/:id - to fetch job details including bids and comments.
 // POST /jobs/:id/bid - for contractors to bid on a job.
 // POST /jobs/:id/comment - to add comments on a job.
+
+// export default router;
