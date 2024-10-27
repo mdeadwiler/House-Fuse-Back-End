6614e1c0c18a6faf6d3ab34bfd510d0e227791e8
George-25 <georgemallory26@gmail.com>
2024-10-26 20:03:27 -0400

    comment.js and seeddata done

--- routes/contractor.js
+++ routes/contractor.js
@@ -1,20 +1,23 @@
 import { Router } from "express";
-import * as controllers from "../controllers/contractors.js"
+import * as controllers from "../controllers/contractors.js";
+import * as comment from "../controllers/comment.js"
 
 
 const router = Router();
 
 // POST /homeowners - for creating a homeowner.
+
 // POST /contractors - for creating a contractor.
 
 
 // POST /jobs - for homeowners to create a job post.
-router.post("/jobs", controllers.createJobPost);
+router.post("/", controllers.createJobPost);
 
 // GET /jobs/:id - to fetch job details including bids and comments.
-router.get("/jobs/:id", controllers.getJobPost);
+router.get("/:id", controllers.getJobPost);
 
 // POST /jobs/:id/bid - for contractors to bid on a job.
 // POST /jobs/:id/comment - to add comments on a job.
+router.post("/:id/comment", comment.createComment)
 
 export default router;
