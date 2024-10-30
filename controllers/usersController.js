8c77043e09521a8ded0e37b28d981a8bb41a367a
MDeadwiler <mdeadwiler@email.com>
2024-10-30 14:21:02 -0400

    hashedPassword-movedup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,93 +1,108 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
-
-
-// This is updated from dev branch AFTER FIXING FROM COMMONJS TO (ECMASCRIPT 6)ES6 SYNTAX 
+// This is updated from dev branch AFTER FIXING FROM COMMONJS TO (ECMASCRIPT 6)ES6 SYNTAX
 
 // Signup route
-export const signup = ("/signup", async (req, res) => {
-  try {
-    const { username, password , email, firstName, lastName, isHomeOwner, contractorCompany, contractorCategory } = req.body;
+export const signup =
+  ("/signup",
+  async (req, res) => {
+    try {
+      const {
+        username,
+        password,
+        email,
+        firstName,
+        lastName,
+        isHomeOwner,
+        contractorCompany,
+        contractorCategory,
+      } = req.body;
 
-    // Check if email is already taken
-    const existingEmail = await User.findOne({ email });
-    if (existingEmail) {
-      return res.status(400).json({ error: "Email is already taken." });
+      // Check if email is already taken
+      const existingEmail = await User.findOne({ email });
+      if (existingEmail) {
+        return res.status(400).json({ error: "Email is already taken." });
+      }
+
+      // Check if username is already taken
+      const existingUsername = await User.findOne({ username });
+      if (existingUsername) {
+        return res.status(400).json({ error: "Username is already taken." });
+      }
+
+      const hashedPassword = await bcrypt.hash(
+        password,
+        parseInt(process.env.SALT_ROUNDS)
+      );
+
+      const userData = {
+        username,
+        hashedPassword,
+        email,
+        firstName,
+        lastName,
+        isHomeOwner,
+      };
+
+      if (!isHomeOwner) {
+        userData.contractorCompany = contractorCompany;
+        userData.contractorCategory = contractorCategory;
+      }
+
+      // Create a new user with isHomeowner field
+      const newUser = await User.create(userData);
+
+      // Generate JWT for the new user (without expiration)
+      const token = jwt.sign(
+        {
+          username: newUser.username,
+          _id: newUser._id,
+          isHomeOwner: newUser.isHomeOwner,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.status(201).json({ token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
-
-    // Check if username is already taken
-    const existingUsername = await User.findOne({ username });
-    if (existingUsername) {
-      return res.status(400).json({ error: "Username is already taken." });
-    }
-
-   const userData = {
-    username,
-    hashedPassword,
-    email,
-    firstName,
-    lastName,
-    isHomeOwner,
-   }
-
-   if (!isHomeOwner){
-    userData.contractorCompany = contractorCompany
-    userData.contractorCategory = contractorCategory
-   }
-
-    // Create a new user with isHomeowner field
-    const newUser = await User.create(userData);
-
-   const hashedPassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS))
-   
-
-    // Generate JWT for the new user (without expiration)
-    const token = jwt.sign(
-      {
-        username: newUser.username,
-        _id: newUser._id,
-        isHomeOwner: newUser.isHomeOwner,
-      },
-      process.env.JWT_SECRET
-    );
-
-    res.status(201).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+  });
 
 // Signin route
-export const signin = ("/signin", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+export const signin =
+  ("/signin",
+  async (req, res) => {
+    try {
+      const { username, password } = req.body;
 
-    // Check if the username exists
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ error: "Username not found." });
+      // Check if the username exists
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ error: "Username not found." });
+      }
+
+      // Check if the password matches
+      const isMatch = bcrypt.compareSync(password, user.hashedPassword);
+      if (!isMatch) {
+        return res.status(400).json({ error: "Incorrect password." });
+      }
+
+      // Generate JWT for the user (without expiration)
+      const token = jwt.sign(
+        {
+          username: user.username,
+          _id: user._id,
+          isHomeOwner: user.isHomeOwner,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
-
-    // Check if the password matches
-    const isMatch = bcrypt.compareSync(password, user.hashedPassword);
-    if (!isMatch) {
-      return res.status(400).json({ error: "Incorrect password." });
-    }
-
-    // Generate JWT for the user (without expiration)
-    const token = jwt.sign(
-      { username: user.username, _id: user._id, isHomeOwner: user.isHomeOwner },
-      process.env.JWT_SECRET
-    );
-
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-
+  });
