43372ec33fe0b7c39d236cc30afc1f3666814bdf
MDeadwiler <mdeadwiler@email.com>
2024-10-31 09:29:11 -0400

    added comments route individual file

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import authorizedUser from "../middleware/authorized-user.js";
+import * as commentControllers from "../controllers/commentController.js";
+
+const router = Router();
+
+
+
+router.delete("/:commentId", authorizedUser, commentControllers.deleteComment); // DELETE /api/comments/:commentId => Deletes Comment. [params]
+router.put("/:commentId", authorizedUser, commentControllers.updateComment); // PUT /api/comments/:commentId => Updates a Comment. [params, body]
+
+export default Router;
